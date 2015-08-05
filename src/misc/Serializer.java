86517002b770b73d654b7b67816128f6e52f6483
memoryLeak47 <memoryLeak47@protonmail.ch>
2015-08-05 16:16:06 +0200

    worked on misc.Serializer

--- src/misc/Serializer.java
+++ src/misc/Serializer.java
@@ -1,1 +1,31 @@
 package misc;
+
+import java.io.*;
+import java.util.*;
+
+public final class Serializer
+{
+	private Serializer() {}
+
+	public static byte[] objectToByteArray(Object object) // serialize
+	{
+		try
+		{
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			ObjectOutputStream out = new ObjectOutputStream(bos);
+			out.writeObject(object);
+			out.close();
+			return bos.toByteArray();
+		} catch (Exception e) {System.out.println(e.getMessage());}
+		return null;
+	}
+
+	public static Object byteArrayToObject(byte[] bytes) // deserialize
+	{
+		try (ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bytes)))
+		{
+			return in.readObject();
+		} catch (Exception e){System.out.println(e.getMessage());}
+		return null;
+	}
+}
