7b430cff51da7e479f19b4ce9b761b3f32bdd211
Bluemi <alok-musik@arcor.de>
2015-08-06 11:25:29 +0200

    core.Main Timer added

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,14 +1,45 @@
 package core;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import core.menu.MenuList;
+
 public class Main
 {
+	public static final int FRAME_INTERVAL = 40;
+
+	private static MenuList menuList;
+	// private NetworkDevice networkDevice;
+
 	public static void main(String args[])
 	{
-		run();
+		new Main().run();
 	}
 
 	public void run()
 	{
+		Timer timer = new Timer();
+
+		timer.scheduleAtFixedRate(new TimerTask()
+		{
+			@Override
+			public void run()
+			{
+				tick();
+				render();
+			}
+		}, FRAME_INTERVAL, FRAME_INTERVAL);
+	}
+
+	public void tick()
+	{
 		
+	}
+
+	public void render()
+	{
+		menuList.render();
+		Screen.update();
 	}
 }
