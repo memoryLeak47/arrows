abcfe64a7c0ff364a45edc750d90e3252c3ee2e1
Bluemi <alok-musik@arcor.de>
2015-08-06 10:18:59 +0200

    Was soll das?

---
+++ src/core/menu/MenuList.java
@@ -0,0 +1,20 @@
+package core.menu;
+
+import java.util.LinkedList;
+
+import core.menu.Menu;
+
+public class MenuList
+{
+	private LinkedList<Menu> menuList;
+
+	public void tick()
+	{
+		getTopMenu().tick();
+	}
+
+	public Menu getTopMenu()
+	{
+		return menuList.getLast();
+	}
+}
