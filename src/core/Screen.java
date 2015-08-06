c9a561ffefe2dd9e5ff7b346898deed707619451
memoryLeak47 <memoryLeak47@protonmail.ch>
2015-08-06 11:05:03 +0200

    added core.Screen

---
+++ src/core/Screen.java
@@ -0,0 +1,47 @@
+package core;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+
+import core.Game;
+
+public class Screen extends JPanel
+{
+	public static final int CAM_OFFSET = 16;
+	public static int camXOffset = 0, camYOffset = 0;
+	private static JFrame frame;
+	private static Screen instance = new Screen(); 
+	private static BufferedImage buffer;
+	private static Graphics g;
+
+	private Screen()
+	{
+		// init stuff
+		frame = new JFrame("Dungeon");
+		frame.add(this);
+		frame.setSize(/*SW*/, /*SH*/);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		buffer = new BufferedImage(/*SW*/, /*SH*/, BufferedImage.TYPE_INT_ARGB);
+		g = buffer.getGraphics();
+		frame.setFocusable(true);
+		setFocusable(true);
+		requestFocusInWindow();
+	}
+
+	static void update()
+	{
+		instance.getGraphics().drawImage(buffer, 0, 0, /*SW*/, /*SH*/, null); // renders buffer on screen
+		g.dispose(); // disposes g
+		g = buffer.getGraphics(); // setups g (used in Screen.draw(...))
+		g.setColor(Color.BLACK); // clears ...
+		g.fillRect(0, 0, /*SW*/, /*SH*/); // ... screen
+	}
+
+	public static Graphics g() { return g; }
+	public static Screen get() { return instance; }
+}
