4a3cb7ae9fac3fe98279fec079be63c36a5305fe
Bluemi <alok-musik@arcor.de>
2015-08-25 21:15:21 +0200

    added core.menu.components.PlayerIcon

---
+++ src/core/menu/components/PlayerIcon.java
@@ -0,0 +1,28 @@
+package core.menu.components;
+
+import java.awt.Color;
+
+import core.menu.MenuComponent;
+import network.lobby.LobbyPlayer;
+import core.Screen;
+
+public class PlayerIcon extends MenuComponent
+{
+	private LobbyPlayer player;
+
+	public PlayerIcon(Menu menu, Rect rect)
+	{
+		super(menu, rect);
+	}
+
+	public void setPlayer(LobbyPlayer player)
+	{
+		this.player = player;
+	}
+
+	public void render()
+	{
+		Screen.g().setColor(Color.YELLOW);
+		Screen.g().fillRect(getPosition().getX(), getPosition().getY(), getSize().getX(), getSize().getY());
+	}
+}
