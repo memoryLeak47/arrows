2dafcffabb22c2e52f29ae185636efd957959d62
Bluemi <alok-musik@arcor.de>
2015-12-28 20:47:58 +0100

    added Camera.gamePositionToScreenPosition and Camera.gameSizeToScreenSize

--- src/misc/game/Camera.java
+++ src/misc/game/Camera.java
@@ -2,12 +2,15 @@ package misc.game;
 
 import static core.Main.INGAME_SCREENSIZE_X;
 import static core.Main.INGAME_SCREENSIZE_Y;
+import static core.Main.TILESIZE;
 import core.Main;
 import entity.Entity;
 import misc.math.game.GamePoint;
 import misc.math.game.GamePosition;
 import misc.math.game.GameRect;
 import misc.math.game.GameSize;
+import misc.math.menu.MenuPosition;
+import misc.math.menu.MenuSize;
 import misc.Debug;
 import network.game.player.GamePlayer;
 
@@ -32,5 +35,22 @@ public class Camera
 	public GameRect getRect()
 	{
 		return new GameRect(getOffset(), new GameSize(INGAME_SCREENSIZE_X, INGAME_SCREENSIZE_Y));
+	}
+
+	public MenuPosition gamePositionToScreenPosition(GamePosition position)
+	{
+		Debug.warnIf(position == null, "Camera.gamePositionToScreenPosition(): position == null");
+
+		position.subtract(getOffset());
+		position.scale(TILESIZE);
+		return new MenuPosition((int) position.getX(), (int) position.getY());
+	}
+
+	public MenuSize gameSizeToScreenSize(GameSize size)
+	{
+		Debug.warnIf(size == null, "Camera.gameSizeToScreenSize(): size == null");
+
+		size.scale(TILESIZE);
+		return new MenuSize((int) size.getX(), (int) size.getY());
 	}
 }
