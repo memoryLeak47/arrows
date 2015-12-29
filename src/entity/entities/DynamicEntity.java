b4e0887561ad18da4358fd323bf1f827add2669e
Bluemi <alok-musik@arcor.de>
2015-12-29 16:48:54 +0100

    added DynamicEntity.getPossibleColliders()

--- src/entity/entities/DynamicEntity.java
+++ src/entity/entities/DynamicEntity.java
@@ -32,5 +32,46 @@ public abstract class DynamicEntity extends Entity
 		accelerate(new GameVector(x, y));
 	}
 
+	// abstract
+	protected abstract boolean isCollidingTiles();
+	protected abstract boolean isCollidingPlayers();
+	protected abstract boolean isCollidingBullets();
+
+	// getter
+	// gibt nur die Tiles zurück, die 
+	protected final LinkedList<Entity> getPossibleColliders()
+	{
+		LinkedList<Entity> possibleColliders = new LinkedList<Entity>();
+
+		if (isCollidingTiles())
+		{
+			for (ExtendedTile tile : GameTileMap.getPossibleColliders())
+			{
+				possibleColliders.add(tile);
+			}
+		}
+		if (isCollidingPlayers())
+		{
+			for (ServerGamePlayer player : Game.get().getPlayers())
+			{
+				if (this != player)
+				{
+					possibleColliders.add(player);
+				}
+			}
+		}
+		if (isCollidingBullets())
+		{
+			for (ExtendedBullet bullet : Game.get().getBullets())
+			{
+				if (this != bullet)
+				{
+					possibleColliders.add(bullet);
+				}
+			}
+		}
+		return possibleColliders;
+	}
+
 	public GameVector getVelocity() { return velocity; }
 }
