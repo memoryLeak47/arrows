3e5cf02ac6b036e15d192be9d2f033b357aa9871
memoryleak47 <memoryleak47@protonmail.ch>
2015-12-30 22:47:51 +0100

    Packet.MAX_SIZE is now at 10000 --> default.png is a valid map

--- src/network/Packet.java
+++ src/network/Packet.java
@@ -4,5 +4,5 @@ import java.io.Serializable;
 
 public class Packet implements Serializable // Superclass of all Packets
 {
-	public static final int MAX_SIZE = 2048; // maximum size of a packet
+	public static final int MAX_SIZE = 10000; // maximum size of a packet
 }
