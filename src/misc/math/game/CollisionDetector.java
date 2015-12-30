908aae87d73d9d1279042031c7f728483fa9ce3d
memoryleak47 <memoryleak47@protonmail.ch>
2015-12-30 14:33:13 +0100

    optimized tile-collision

--- src/misc/math/game/CollisionDetector.java
+++ src/misc/math/game/CollisionDetector.java
@@ -8,7 +8,8 @@ import misc.Debug;
 
 public final class CollisionDetector
 {
-	private static final float VALENCE = 0.2f;
+	private static final float POSITION_VALENCE = 0.25f; // 0.15f
+	private static final float SPEED_VALENCE = 0.002f; // 0.002f
 
 	private CollisionDetector() {}
 
@@ -141,36 +142,36 @@ public final class CollisionDetector
 	public static boolean collideTileRight(DynamicEntity e, ExtendedTile t) // Kollidiere ich mit dem Tile rechts von mir
 	{
 		//     Wenn ich mich nach rechts bewege & wenn ich mich links vom Tile befinde
-		return (e.getOldVelocity().getX() >= 0) 
+		return (e.getOldVelocity().getX() > -SPEED_VALENCE) 
 			&& (e.getPosition().getX() < t.getPosition().getX())
-			&& (t.getBot() - VALENCE > e.getTop())
-			&& (t.getTop() + VALENCE < e.getBot());
+			&& (t.getBot() - POSITION_VALENCE > e.getTop())
+			&& (t.getTop() + POSITION_VALENCE < e.getBot());
 	}
 
 	public static boolean collideTileLeft(DynamicEntity e, ExtendedTile t) // Kollidiere ich mit dem Tile links von mir
 	{
 		//     Wenn ich mich nach links bewege & wenn ich mich rechts vom Tile befinde
-		return (e.getOldVelocity().getX() <= 0) // Wenn ich mich nach links bewege
+		return (e.getOldVelocity().getX() < SPEED_VALENCE) // Wenn ich mich nach links bewege
 			&& (e.getPosition().getX() > t.getPosition().getX()) // wenn ich mich rechts vom Tile befinde
-			&& (t.getBot() - VALENCE > e.getTop())
-			&& (t.getTop() + VALENCE < e.getBot());
+			&& (t.getBot() - POSITION_VALENCE > e.getTop())
+			&& (t.getTop() + POSITION_VALENCE < e.getBot());
 	}
 
 	public static boolean collideTileTop(DynamicEntity e, ExtendedTile t) // Kollidiere ich mit dem Tile oben von mir
 	{
 		//     Wenn ich mich nach oben bewege & wenn ich mich unten vom Tile befinde
-		return (e.getOldVelocity().getY() <= 0) // Wenn ich mich nach oben bewege
+		return (e.getOldVelocity().getY() < SPEED_VALENCE) // Wenn ich mich nach oben bewege
 			&& (e.getPosition().getY() > t.getPosition().getY()) // wenn ich mich unten vom Tile befinde
-			&& (t.getRight() - VALENCE > e.getLeft())
-			&& (t.getLeft() + VALENCE < e.getRight());
+			&& (t.getRight() - POSITION_VALENCE > e.getLeft())
+			&& (t.getLeft() + POSITION_VALENCE < e.getRight());
 	}
 
 	public static boolean collideTileBot(DynamicEntity e, ExtendedTile t) // Kollidiere ich mit dem Tile unten von mir
 	{
 		//     Wenn ich mich nach unten bewege & wenn ich mich oben vom Tile befinde
-		return (e.getOldVelocity().getY() >= 0)
+		return (e.getOldVelocity().getY() > -SPEED_VALENCE)
 			&& (e.getPosition().getY() < t.getPosition().getY())
-			&& (t.getRight() - VALENCE > e.getLeft())
-			&& (t.getLeft() + VALENCE < e.getRight());
+			&& (t.getRight() - POSITION_VALENCE > e.getLeft())
+			&& (t.getLeft() + POSITION_VALENCE < e.getRight());
 	}
 }
