4f27fc764049083c9913ded057a8894bb2c708b3
Bluemi <alok-musik@arcor.de>
2016-01-03 22:05:35 +0100

    new Design (Darkmode :3), lobbyrun now works for my slow PC(mapsleepiness)

--- src/core/Main.java
+++ src/core/Main.java
@@ -17,8 +17,8 @@ import network.NetworkDevice;
 public class Main
 {
 	public static final int FRAME_INTERVAL = 40; // intervall in dem tick() und render() ausgeführt werden
+	public static final int TILESIZE = 40;
 
-	public static final int TILESIZE = 36;
 	public static final float DRAG_X = 1.9f;
 	public static final float DRAG_Y = 1.06f;
 	public static final float GRAVITY = 0.04f;
