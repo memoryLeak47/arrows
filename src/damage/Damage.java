88797a7f9745a9f6d4cf1f55af0876878e6164e5
memoryleak47 <memoryleak47@protonmail.ch>
2016-01-10 14:07:15 +0100

    worked on damage.Damage

--- src/damage/Damage.java
+++ src/damage/Damage.java
@@ -2,12 +2,16 @@ package damage;
 
 public class Damage
 {
+	private int hit, cut, magic;
+
 	public Damage(int hit, int cut, int magic)
 	{
 		this.hit = hit;
 		this.cut = cut;
 		this.magic = magic;
 	}
-	
-	public int hit, cut, magic;
+
+	public int getHit() { return hit; }
+	public int getCut() { return cut; }
+	public int getMagic() { return magic; }
 }
