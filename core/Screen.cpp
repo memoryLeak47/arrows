4b08189cacfc231357053188daff303c04ca2913
memoryleak47 <memoryleak47@protonmail.ch>
2016-01-27 20:24:27 +0100

    added window to Screen

--- core/Screen.cpp
+++ core/Screen.cpp
@@ -1,6 +1,8 @@
 #include "Screen.h"
 
+sf::RenderWindow* Screen::window;
+
 void Screen::init()
 {
-	
+	Screen::window = new sf::RenderWindow(sf::VideoMode(800, 600), "Arrows");
 }
--- core/Screen.h
+++ core/Screen.h
@@ -1,10 +1,16 @@
 #ifndef __SCREEN_CLASS__
 #define __SCREEN_CLASS__
 
+#include <SFML/Graphics.hpp>
+
 class Screen
 {
+	private:
+		static sf::RenderWindow *window;
 	public:
 		static void init();
 };
+
+
 
 #endif
