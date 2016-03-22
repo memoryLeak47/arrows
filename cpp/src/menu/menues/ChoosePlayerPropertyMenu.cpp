08ef4317b72009919e6e855327252150b3d88d78
memoryleak47 <memoryleak47@protonmail.ch>
2016-03-22 23:12:16 +0100

    ChoosePlayerPropertyMenu'es SlotIcons now contain the SlotIcons defined in slotPacket'

--- cpp/src/menu/menues/ChoosePlayerPropertyMenu.cpp
+++ cpp/src/menu/menues/ChoosePlayerPropertyMenu.cpp
@@ -9,7 +9,7 @@
 class SlotIcon : public PlayerPropertyIcon
 {
 	public:
-		SlotIcon(ChoosePlayerPropertyMenu* c, const PixelRect& r) : PlayerPropertyIcon(c, r) {}
+		SlotIcon(ChoosePlayerPropertyMenu* c, PlayerProperty* p, const PixelRect& r) : PlayerPropertyIcon(c, r, p) {}
 		virtual void onClick(int mouseButton) override
 		{
 			setPlayerProperty(NULL);
@@ -49,7 +49,7 @@ ChoosePlayerPropertyMenu::ChoosePlayerPropertyMenu(LobbyMenu* lobbyMenu, PlayerP
 	// Slot Icons
 	for (int i = 0; i < slotPacket->getPlayerProperties().size(); i++)
 	{
-		PlayerPropertyIcon* icon = new SlotIcon(this, PixelRect(60 + i * (PlayerPropertyIcon::getSize().getX() + 15), 40, PlayerPropertyIcon::getSize().getX(), PlayerPropertyIcon::getSize().getY()));
+		PlayerPropertyIcon* icon = new SlotIcon(this, slotPacket->getPlayerProperties()[i], PixelRect(60 + i * (PlayerPropertyIcon::getSize().getX() + 15), 40, PlayerPropertyIcon::getSize().getX(), PlayerPropertyIcon::getSize().getY()));
 		addComponent(icon);
 		slotIcons.push_back(icon);
 	}
