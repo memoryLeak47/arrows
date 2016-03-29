2595ef67acfd953f593799826cd2a5dd6d331702
memoryleak47 <memoryleak47@protonmail.ch>
2016-03-30 00:36:23 +0200

    implemented GameInterfaces Entity-ticking/collision-checking structure

--- cpp/src/game/GameInterface.cpp
+++ cpp/src/game/GameInterface.cpp
@@ -25,4 +25,52 @@ GameInterface::~GameInterface()
 void GameInterface::tick()
 {
 	NetworkingMenu::tick();
+	for (unsigned int i = 0; i < mobs.size(); i++)
+	{
+		mobs[i]->tick();
+	}
+	// TODO tick tiles?
+	for (unsigned int i = 0; i < bullets.size(); i++)
+	{
+		bullets[i]->tick();
+	}
+
+	for (unsigned int i = 0; i < mobs.size(); i++)
+	{
+		mobs[i]->calculateCollisions(mobs, tiles, bullets);
+	}
+	for (unsigned int i = 0; i < tiles.size(); i++)
+	{
+		tiles[i]->calculateCollisions(mobs, tiles, bullets);
+	}
+	for (unsigned int i = 0; i < bullets.size(); i++)
+	{
+		bullets[i]->calculateCollisions(mobs, tiles, bullets);
+	}
+
+	for (unsigned int i = 0; i < mobs.size(); i++)
+	{
+		mobs[i]->calculateForces();
+	}
+	for (unsigned int i = 0; i < tiles.size(); i++)
+	{
+		tiles[i]->calculateForces();
+	}
+	for (unsigned int i = 0; i < bullets.size(); i++)
+	{
+		bullets[i]->calculateForces();
+	}
+
+	for (unsigned int i = 0; i < mobs.size(); i++)
+	{
+		mobs[i]->applyForces();
+	}
+	for (unsigned int i = 0; i < tiles.size(); i++)
+	{
+		tiles[i]->applyForces();
+	}
+	for (unsigned int i = 0; i < bullets.size(); i++)
+	{
+		bullets[i]->applyForces();
+	}
 }
