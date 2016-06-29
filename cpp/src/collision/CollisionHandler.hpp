732b1accc7385efbb5f3a0329e40287080239ec1
memoryleak47 <memoryleak47@protonmail.ch>
2016-06-29 18:19:59 +0200

    hardly planned CollisionHandler

--- cpp/src/collision/CollisionHandler.hpp
+++ cpp/src/collision/CollisionHandler.hpp
@@ -1,3 +1,10 @@
+/*
+	handleCollisionEvent is only called for solid collisions (and maybe slow-collision or something)
+	It prevents the glitching of the entities into each other
+	It applies a drag
+	It pushes the entities away from each other by sponginess
+*/
+
 #ifndef __COLLISIONHANDLER_CLASS__
 #define __COLLISIONHANDLER_CLASS__
 
