f2a371ffb4c860f48aeff679ee8ba66e55842308
Bluemi <bruno.schilling@protonmail.ch>
2016-07-01 13:53:16 +0200

    planned calculation of Fluchtvec in CollisionHandler

--- cpp/src/collision/CollisionHandler.hpp
+++ cpp/src/collision/CollisionHandler.hpp
@@ -1,8 +1,17 @@
 /*
-	handleCollisionEvent is only called for solid collisions (and maybe slow-collision or something)
-	It prevents the glitching of the entities into each other
-	It applies a drag
-	It pushes the entities away from each other by sponginess
+	CollisionHandler is called whenever a Collision happens
+	solid:
+		It prevents the glitching of the entities into each other
+		It applies a drag
+		It pushes the entities away from each other by sponginess
+
+	Berechnung des Fluchtvectors:
+		Seien A, B die CollisionsPunkte der Entity E1 bei der Collision mit der Entity E2.
+		Man bezeichne den Vector zwischen A und B mit AB = B - A
+		Der Vector O sei der Vector AB um 90° gedreht (Die Richtung in die gedreht wird, spielt keine Rolle)
+		Der Vector M sei der Mittelpunkt der Entity E1
+		Der Vector AM sei gegeben durch AM = M - A
+		Der gesuchte Fluchtvektor F ist gegeben durch F = O * ( AM · O ) // noch nicht normalisiert
 */
 
 #ifndef __COLLISIONHANDLER_CLASS__
