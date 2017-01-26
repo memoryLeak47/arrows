4b9f5d228d03cc4a2573e725cc918dada539e5e2
memoryleak47 <memoryleak47@protonmail.ch>
2017-01-26 12:22:20 +0100

    *precompiler.py

--- cpp/precompiler.py
+++ cpp/precompiler.py
@@ -2,6 +2,10 @@
 
 import sys
 import glob
+import string
+
+class Error: pass
+E=Error()
 
 def die(msg):
 	print("ERROR: " + msg)
@@ -51,43 +55,43 @@ def walk_blank(filestr, index): # => (blank, index)
 	# allowes blank == ""
 	blank=""
 	while filestr[index] in [" ", "\n", "\t"]:
-		blank.append(filestr[index])
+		blank += filestr[index]
 		index += 1
 	return blank, index
 
 def walk_simple_typename(filestr, index): # => (typename, index)
 	if filestr[index] in "0123456789":
-		return "ERROR", "ERROR"
+		return E, E
 
 	typename = ""
-	while filestr[index] in (strings.alphabet + strings.Alphabet + "_0123456789"):
-		typename.append(filestr[index])
+	while filestr[index] in (string.ascii_lowercase + string.ascii_uppercase + "_0123456789"):
+		typename += filestr[index]
 		index += 1
 
 	if typename == "":
-		return "ERROR", "ERROR"
+		return E, E
 
 	return typename, index
 
 def walk_spaced_typename(filestr, index): # => (typename, index)
 	typename, index = walk_simple_typename(filestr, index)
-	if typename == "ERROR" or index == "ERROR":
-		return "ERROR", "ERROR"
+	if typename == E or index == E:
+		return E, E
 
 	if filestr[index:].startswith("::"):
 		index += 2
 
 		typename2, index2 = walk_simple_typename(filestr, index)
-		if typename2 == "ERROR" or index2 == "ERROR":
-			return "ERROR", "ERROR"
+		if typename2 == E or index2 == E:
+			return E, E
 
 		return typename + "::" + typename2, index2
 	return typename, index
 
 def walk_typename(filestr, index): # => (typename, index)
 	typename, index = walk_spaced_typename(filestr, index)
-	if typename == "ERROR" or index == "ERROR":
-		return "ERROR"
+	if typename == E or index == E:
+		return E, E
 
 	if filestr[index] != "<":
 		return typename, index
@@ -96,8 +100,8 @@ def walk_typename(filestr, index): # => (typename, index)
 	typenames = list()
 	while True:
 		typename_tmp, index = walk_typename(filestr, index)
-		if typename_tmp == "ERROR" or index == "ERROR":
-			return "ERROR", "ERROR"
+		if typename_tmp == E or index == E:
+			return E, E
 		typenames.append(typename_tmp)
 		index = walk_blank(filestr, index)
 		if filestr[index] == ",":
@@ -116,9 +120,22 @@ def walk_modifier(filestr, index):  # => (modifier, index)
 	elif filestr[index].startswith("private"):
 		return "private", index + 7
 	else:
-		return "ERROR", "ERROR"
+		return E, E
 
-def find_class_def(classname, files):
+def walk_to(pattern, filestr, index): # => index
+	while True:
+		if filestr[index:].startswith(pattern):
+			return index
+		elif filestr[index:].startswith("//"):
+			index = filestr.find("\n", index)
+		elif filestr[index:].startswith("/*"):
+			index = filestr.find("*/", index+2)
+		else:
+			index += 1
+			if index >= len(filestr):
+				return E
+
+def find_class_def(classname, files): # (file, index)
 	for file in files.filenames():
 		filestr = files[file]
 		index = -1 # because of index+1
@@ -127,18 +144,24 @@ def find_class_def(classname, files):
 			if index == -1:
 				break
 			else:
+				index += 5
 				blank, index = walk_blank(filestr, index)
 				if blank == "":
 					continue
 				typename, index = walk_typename(filestr, index)
 				if typename != classname:
 					continue
-				assert(index != "ERROR")
-				# TODO
+				assert(index != E)
+				_, index = walk_blank(filestr, index)
+				index = walk_to("{", filestr, index)
+				if index == E:
+					return E, E
+				return file, index
+	return E, E
 
 def main():
 	files = Files()
-	# TODO
+	print(find_class_def("Main", files))
 	files.writeback()
 
 main()
