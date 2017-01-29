15243c322444f64f592f63a368ec2ffa9a1e3b08
memoryleak47 <memoryleak47@protonmail.ch>
2017-01-29 18:22:34 +0100

    ...

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,17 +1,7 @@
-// #include "core/Main.hpp"
-#include "game/Frame.hpp"
-#include "entity/TestKiste.hpp"
-#include <iostream>
+#include "core/Main.hpp"
 
 int main()
 {
-	// Main main;
-	TestKiste* tk = new TestKiste(GameVector(0,0));
-	std::cout << tk->to_string() << std::endl;
-
-	// Frame f;
-	// f.idlers.push_back(tk);
-	// f.clone();
-
+	Main main;
 	return 0;
 }
