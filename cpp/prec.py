597da5797873dabce678589238515328044c3c2a
memoryleak47 <memoryleak47@protonmail.ch>
2017-03-21 18:59:03 +0100

    prec.py now knows what a newline is :)

--- cpp/prec.py
+++ cpp/prec.py
@@ -21,32 +21,32 @@ for subclass in frame_cloneables:
 	cpp = s.file.replace(".hpp", ".cpp")
 
 	# add copy-constructor
-	add_to_class_def(subclass, "public: " + subclass + "(const " + subclass + "&) = default; private: ", files)
+	add_to_class_def(subclass, "public:\n" + subclass + "(const " + subclass + "&) = default;\nprivate:\n", files)
 
 	# add clone()
 	if not is_surrounded_by_markername(s.index, s.file, "abstract", files):
-		add_to_class_def(subclass, "public: FrameCloneable* clone(std::map<FrameCloneable*, FrameCloneable*>*) const override; private: ", files)
-		string = "FrameCloneable* " + subclass + "::clone(std::map<FrameCloneable*, FrameCloneable*>* map) const { " + subclass + " *c = new " + subclass + "(*this); c->cloneMembers(map); return c; }"
+		add_to_class_def(subclass, "public:\nFrameCloneable* clone(std::map<FrameCloneable*, FrameCloneable*>*) const override;\nprivate:\n", files)
+		string = "FrameCloneable* " + subclass + "::clone(std::map<FrameCloneable*, FrameCloneable*>* map) const\n{\n" + subclass + " *c = new " + subclass + "(*this);\nc->cloneMembers(map);\nreturn c;\n}\n"
 		add_to_file(cpp, string, files)
 
 	# add cloneMembers()
-	add_to_class_def(subclass, "protected: void cloneMembers(std::map<FrameCloneable*, FrameCloneable*>*); private: ", files)
-	string = "void " + subclass + "::cloneMembers(std::map<FrameCloneable*, FrameCloneable*>* map) {"
+	add_to_class_def(subclass, "protected:\nvoid cloneMembers(std::map<FrameCloneable*, FrameCloneable*>*);\nprivate:\n", files)
+	string = "void " + subclass + "::cloneMembers(std::map<FrameCloneable*, FrameCloneable*>* map)\n{\n"
 	for supername in s.superstructures:
 		superstructure = files.structuredict[supername]
 		if superstructure.name in frame_cloneables:
-			string += superstructure.name + "::cloneMembers(map);"
+			string += superstructure.name + "::cloneMembers(map);\n"
 	l = s.get_member_markers(files)
 	if "clone" in l:
 		for marker in l["clone"]:
 			content = get_marker_content(marker, s.file, files)
-			string += "if (map->find(" + content + ") == map->end()) { (*map)[" + content + "] = " + content + "->clone(map); } " + content + " = static_cast<decltype(" + content + ")>((*map)[" + content + "]); "
+			string += "if (map->find(" + content + ") == map->end())\n{\n(*map)[" + content + "] = " + content + "->clone(map);\n}\n" + content + " = static_cast<decltype(" + content + ")>((*map)[" + content + "]);\n"
 	if "clone_list" in l:
 		for marker in l["clone_list"]:
 			content = get_marker_content(marker, s.file, files)
-			string += "for (unsigned int i = 0; i < " + content + ".size(); i++) { if (map->find(" + content + "[i]) == map->end()) { (*map)[" + content + "[i]] = " + content + "[i]->clone(map); } " + content + "[i] = static_cast<decltype(" + content + ")::value_type>((*map)[" + content + "[i]]); }"
+			string += "for (unsigned int i = 0; i < " + content + ".size(); i++)\n{\nif (map->find(" + content + "[i]) == map->end())\n{\n(*map)[" + content + "[i]] = " + content + "[i]->clone(map);\n}\n" + content + "[i] = static_cast<decltype(" + content + ")::value_type>((*map)[" + content + "[i]]);\n}\n"
 
-	string += "}"
+	string += "}\n"
 	add_to_file(cpp, string, files)
 # 
 files.writeback()
